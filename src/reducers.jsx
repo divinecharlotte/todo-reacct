0d9ad6528fb763492da086fc71c9ef6f8c28940f
charlottedivine <divinemaina@gmail.com>
2023-11-09 11:26:31 +0200

    todo with redux

---
+++ src/reducers.jsx
@@ -0,0 +1,34 @@
+import { ADD_TODO, REMOVE_TODO, UPDATE_TODO } from './actions';
+
+const initialState = {
+  todos: []
+};
+
+const todoReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_TODO:
+      return {
+        todos: [
+          ...state.todos,
+          {
+            id: Date.now(),
+            text: action.payload.text
+          }
+        ]
+      };
+    case REMOVE_TODO:
+      return {
+        todos: state.todos.filter(todo => todo.id !== action.payload.id)
+      };
+    case UPDATE_TODO:
+      return {
+        todos: state.todos.map(todo =>
+          todo.id === action.payload.id ? { ...todo, text: action.payload.text } : todo
+        )
+      };
+    default:
+      return state;
+  }
+};
+
+export default todoReducer;
